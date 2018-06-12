7b16d13378ea8a99757983bd99810d62159462a7
CassieMai <xiaochunmai@gmail.com>
2018-06-12 14:17:36 +0800

    for upload local work to github

---
+++ tools/draft.py
@@ -0,0 +1,27 @@
+import numpy as np
+import tensorflow as tf
+import math
+
+# print(np.zeros((2,2,3,1)))
+
+# sess = tf.Session()
+# data = tf.constant([[[[ 1.],[ 2.],[ 3.]],
+#                      [[ 4.],[ 5.],[ 6.]]],
+#                     [[[ 7.],[ 8.],[ 9.]],
+#                      [[ 10.],[ 11.],[ 12.]]]])
+# print(data)
+# d = tf.tile(data, [2,1,1,5])
+# print(d)
+#
+# print(sess.run(d))
+# a = tf.constant([[[[ 0.],[ 2.],[ 4.]],
+#                   [[ 1.],[ 3.],[ 5.]]],
+#                  [[[2.], [4.], [6.]],
+#                   [[3.], [5.], [7.]]]
+#                  ])
+# print(a)
+# d = tf.multiply(d,a)
+# print(sess.run(d))
+
+
+print(int(math.ceil(1/1)))
